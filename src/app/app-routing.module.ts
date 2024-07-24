4c113610d86c691a1dde6822554594383ff56c95
Ismailc23 <mohammed.chenoth@people10.com>
2024-07-24 17:26:06 +0530

    Completed the login and sign up module and adding the jwt token to the header of protected api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { combineLatest } from 'rxjs';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { AdminGuard } from './services/admin.guard';
+import { UserGuard } from './services/user.guard';
 
 const routes: Routes = [
   {
@@ -19,8 +24,21 @@ const routes: Routes = [
     path:'login',
     component:LoginComponent,
     pathMatch:'full'
-  }
+  },
+  {
+    path:'admin',
+    component:DashboardComponent,
+    pathMatch:'full',
+    canActivate:[AdminGuard]
+  },
+  {
+    path:'user-dashboard',
+    component:UserDashboardComponent,
+    pathMatch:'full',
+    canActivate:[UserGuard]
+  },
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUserDto={
+    email:'',
+    password:''
+  }
 
-  ngOnInit(): void {
+  constructor(private loginService:LoginService,private router:Router) { }
+
+  ngOnInit(): void {}
+
+  formSubmit() {
+    console.log("login btn clicked");
+    this.loginService.loginGenerateToken(this.loginUserDto).subscribe(
+      (data:any)=>{
+        console.log("success");
+        console.log(data);
+        this.loginService.loginUser(data.token);
+        this.loginService.getCurrentUser().subscribe(
+          (user:any)=>{
+            this.loginService.setUser(user);
+            console.log(user);
+            if(this.loginService.getUserRole()=="ADMIN")
+            {
+              this.router.navigate(['admin'])
+            }
+            else if(this.loginService.getUserRole()=="USER") {
+              this.router.navigate(['user-dashboard'])
+            }
+            else{
+              this.loginService.logout();
+            }
+          }
+        )
+      },
+      (error)=>{
+        console.log("Error !");
+        console.log(error);
+      }
+    )
   }
 
 }
